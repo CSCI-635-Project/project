1926538579d3cd94251a86fd00532fd04d9663f8
Ben S Giacalone <bsg8294@cs.rit.edu>
2023-12-10 02:33:35 -0500

    Update training script

--- experiments/train_agents.sh
+++ experiments/train_agents.sh
@@ -10,20 +10,34 @@ export SUPPRESS_MA_PROMPT=1
 NUM_UNITS=128
 NUM_EPISODES=25000
 function run_exp() {
-    python train.py --scenario $EXP_NAME --exp-name baseline_$EXP_NAME --save-dir temp/policies/baseline_$EXP_NAME --num-adversaries $NUM_ADV --num-units $NUM_UNITS --num-episodes $NUM_EPISODES
+    if [[ $EXP_TYPE == "baseline" ]]; then
+        PP=""
+    else
+        PP="--preprocess"
+    fi
+    python train.py --scenario $EXP_NAME --exp-name "$EXP_TYPE"_"$EXP_NAME" --save-dir temp/policies/"$EXP_TYPE"_"$EXP_NAME" --num-adversaries $NUM_ADV --num-units $NUM_UNITS --num-episodes $NUM_EPISODES $PP
 }
 
 # Cooperative navigation
 EXP_NAME="simple_spread"
 NUM_ADV=0
+EXP_TYPE="baseline"
+run_exp
+EXP_TYPE="preprocessed"
 run_exp
 
 # Predator-prey
 EXP_NAME="simple_tag"
 NUM_ADV=3
+EXP_TYPE="baseline"
+run_exp
+EXP_TYPE="preprocessed"
 run_exp
 
 # Physical deception
 EXP_NAME="simple_adversary"
 NUM_ADV=1
+EXP_TYPE="baseline"
+run_exp
+EXP_TYPE="preprocessed"
 run_exp
